ec23a6d8139321949e36f30ddfac0918accc446b
Tammy Tan <46847428+tammyt123@users.noreply.github.com>
2021-06-01 16:48:05 -0400

    add functions for DICEFAIR implementation (first pass)

--- src/FAIR_implementation.jl
+++ src/FAIR_implementation.jl
@@ -1,37 +1,57 @@
-### TESTING WHETHER TEMPERATURE OUTPUT FROM FAIR CAN BE IMPLEMENTED IN DICE
+## function to get baseline DICE-FAIR model (i.e. input temperature vector from FAIR-NCEE into baseline DICE 2016)
+function get_dicefair(;usg_scenario::String)
+    
+    ## load baseline DICE model
+    m = MimiDICE2016.get_model()
+    run(m)
 
-## load DICE model
-m = MimiDICE2016.get_model()
-run(m)
+    ## load baseline FAIR-NCEE
+    FAIR = MimiFAIR.get_model(usg_scenario = usg_scenario)
+    run(FAIR)
 
-m[:damages, :TATM] # 100 element array: every 5 years from 2015-2510
-original_cpc = m[:neteconomy, :CPC] # save for comparison
+    fair_years = collect(1765:1:2300)
+    dice2016_years = collect(2015:5:2510)
 
-## FAIR (modified version)
-using MimiFAIR
+    temperature = DataFrame(year = fair_years, T = FAIR[:temperature, :T])
 
-FAIR = MimiFAIR.get_model(usg_scenario = "USG1")
-run(FAIR)
+    input_temp = temperature[[year in dice2016_years for year in fair_years], :T] # 58 element array, needs to be 100. make temperature constant after 2300
+    append!(input_temp, repeat([input_temp[end]], length(m[:damages, :TATM]) - length(input_temp)))
 
-FAIR[:temperature, :T] # annual temperature changes 1765-2300
-fair_years = collect(1765:1:2300)
-dice2016_years = collect(model_years)
+    ## set parameter in DICE model
+    MimiDICE2016.set_param!(m, :TATM, input_temp)
+    run(m)
 
-temperature = DataFrame(year = fair_years, T = FAIR[:temperature, :T])
+    return(m)
 
-input_temp = temperature[[year in dice2016_years for year in fair_years], :T] # 58 element array, needs to be 100. make temperature constant after 2300
-append!(input_temp, repeat([input_temp[end]], length(m[:damages, :TATM]) - length(input_temp)))
+end
 
-## set parameter in DICE model
-set_param!(m, :TATM, input_temp)
+## function to get marginal DICE-FAIR model (i.e. input perturbed FAIR temperature vector into DICE2016)
+## note: FAIR-NCEE must be loaded in environment first!!
+function get_dicefair_marginal_model(;usg_scenario::String, pulse_year::Int)
+    
+    ## create DICE2016 marginal model
+    m = MimiDICE2016.get_dicefair()
+    mm = Mimi.create_marginal_model(m, 1e9)
+    run(mm)
 
-run(m)
-m[:damages, :TATM]
-m[:climatedynamics, :TATM]
-new_cpc = m[:neteconomy, :CPC]
+    ## get perturbed FAIR temperature vector
+    new_temperature = MimiFAIR.get_perturbed_fair_temperature(usg_scenario = usg_scenario, pulse_year = pulse_year)
+    new_temperature_df = DataFrame(year = fair_years, T = new_temperature)
 
-# compare cpc
-dice2016_years[58]
-original_cpc[58]
-new_cpc[58]
+    new_input_temp = new_temperature_df[[year in dice2016_years for year in fair_years], :T] # 58 element array, needs to be 100. make temperature constant after 2300
+    append!(new_input_temp, repeat([new_input_temp[end]], length(model_years) - length(new_input_temp)))
 
+    ## set temperature in marginal DICE model to equal perturbed FAIR temperature
+    MimiDICE2016.update_param!(mm.modified, :TATM, new_input_temp)
+    run(mm)
+
+    return(mm)
+
+end
+
+## compute SCC from DICE-FAIR
+function compute_scc_dicefair(;usg_scenario::String, pulse_year::Int, prtp::Float64, eta::Float64, last_year::Int=2300)
+    mm = MimiDICE2016.get_dicefair_marginal_model(usg_scenario::String, pulse_year::Int)
+    scc = MimiDICE2016._compute_scc(mm, year = pulse_year, last_year = last_year, prtp = prtp, eta = eta)
+    return(scc)
+end
