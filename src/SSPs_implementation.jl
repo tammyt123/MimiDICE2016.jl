73fe5dcbb0af6c86df25618c92ba00a2eb79389b
Tammy Tan <46847428+tammyt123@users.noreply.github.com>
2021-06-24 16:16:30 -0400

    2% growth post 2100 instead of constant gdp and emissions; first pass of SCC calculation

--- src/SSPs_implementation.jl
+++ src/SSPs_implementation.jl
@@ -4,29 +4,38 @@
 
 using Pkg
 Pkg.activate("development")
-using Revise, Mimi, MimiDICE2016, XLSX, Plots, DataFrames 
+using Revise, Mimi, MimiDICE2016, XLSX, Plots, DataFrames
 # using MimiFAIR # make sure MimiFAIR is on the master branch
 
 #--------------------------------------------------------------------------
 # replace YGROSS with SSP GDP
 #--------------------------------------------------------------------------
 
-# get model
+## get model
 m = MimiDICE2016.get_model()
 const model_years = 2015:5:2510
 
-# read in SSP GDP data
+## read in SSP GDP data
 ssp_gdp_file_path = "data/ssps_gdp.xlsx"
 ssp_gdp_data = XLSX.readdata(ssp_gdp_file_path, "data", "G2:X6") # units billion US$2005/yr (PPP), 2015 to 2100
 
-# extend to 2510 (currently only to 2100) -- assume constant after 2100
+## extend to 2510 (currently only to 2100)
+
+# assuming constant after 2100
+# ssp_gdp_data_extended = ssp_gdp_data
+# for i in 1:(length(model_years) - size(ssp_gdp_data)[2])
+#     ssp_gdp_data_extended = hcat(ssp_gdp_data_extended, ssp_gdp_data[:,end])
+# end
+# # ssp_gdp_data_extended
+
+# growing everything at 2% post 2100
 ssp_gdp_data_extended = ssp_gdp_data
 for i in 1:(length(model_years) - size(ssp_gdp_data)[2])
-    ssp_gdp_data_extended = hcat(ssp_gdp_data_extended, ssp_gdp_data[:,end])
+    ssp_gdp_data_extended = hcat(ssp_gdp_data_extended, (ssp_gdp_data_extended[:,end] .* 1.02))
 end
-# ssp_gdp_data_extended
+ssp_gdp_data_extended
 
-# inflate from 2005 to 2010 USD
+## inflate from 2005 to 2010 USD
 inflation_factor = 92.9/81.5 # check this -- using charles' world GDP spreadsheet for now
 ssp_gdp_inflated = convert(Array{Float64}, ssp_gdp_data_extended) .* inflation_factor ./ 1e3 # inflate and convert to trillions
 
@@ -38,21 +47,29 @@ ssp_gdp_dict = Dict("SSP1" => ssp_gdp_inflated[1,:],
 
 input_gdp = ssp_gdp_dict["SSP5"]
 
-# set param
+## set param
 MimiDICE2016.set_param!(m, :YGROSS, input_gdp)
 
 #--------------------------------------------------------------------------
 # replace E with SSP CO2 emissions
 #--------------------------------------------------------------------------
 
-# read in SSP CO2 emissions data
+## read in SSP CO2 emissions data
 ssp_co2_emissions_file_path = "data/ssps_co2_emissions_total.xlsx"
 ssp_co2_emissions_data = XLSX.readdata(ssp_co2_emissions_file_path, "data", "F2:O10") # units billion US$2005/yr (PPP), 2015 to 2100
 
-# extend to 2510 (currently only to 2100) -- assume constant after 2100
+## extend to 2510 (currently only to 2100)
+# # assume constant after 2100
+# ssp_co2_emissions_data_extended = ssp_co2_emissions_data
+# for i in 1:(length(model_years) - size(ssp_co2_emissions_data)[2])
+#     ssp_co2_emissions_data_extended = hcat(ssp_co2_emissions_data_extended, ssp_co2_emissions_data_extended[:,end])
+# end
+# ssp_co2_emissions_data_extended
+
+# decline at constant 2% post-2100
 ssp_co2_emissions_data_extended = ssp_co2_emissions_data
 for i in 1:(length(model_years) - size(ssp_co2_emissions_data)[2])
-    ssp_co2_emissions_data_extended = hcat(ssp_co2_emissions_data_extended, ssp_co2_emissions_data[:,end])
+    ssp_co2_emissions_data_extended = hcat(ssp_co2_emissions_data_extended, ssp_co2_emissions_data_extended[:,end] * 0.98)
 end
 ssp_co2_emissions_data_extended
 
@@ -68,7 +85,7 @@ ssp_co2_emissions_dict = Dict("SSP3-70" => ssp_co2_emissions_data_extended[1,:],
 
 input_co2_emissions = ssp_co2_emissions_dict["SSP5-85"] ./ 1e3 # convert to GtCO2
 
-# set param
+## set param
 MimiDICE2016.set_param!(m, :E, input_co2_emissions)
 
 #--------------------------------------------------------------------------
@@ -101,7 +118,77 @@ plot(model_years, m[:neteconomy, :C], title = "Consumption", label = "SSP5 x DIC
 plot!(model_years, DICE2016[:neteconomy, :C], label = "Native DICE2016")
 
 
+# plot(model_years, m[:grosseconomy, :YGROSS], title = "YGROSS", label = ":grosseconomy", legend = :bottomright)
+# plot!(model_years, m[:neteconomy, :YGROSS], label = ":neteconomy")
+# plot!(model_years, m[:damages, :YGROSS], label = ":damages")
 
-plot(model_years, m[:grosseconomy, :YGROSS], title = "YGROSS", label = ":grosseconomy", legend = :bottomright)
-plot!(model_years, m[:neteconomy, :YGROSS], label = ":neteconomy")
-plot!(model_years, m[:damages, :YGROSS], label = ":damages")
+#--------------------------------------------------------------------------
+# create marginal model
+#--------------------------------------------------------------------------
+
+mm = MimiDICE2016.get_model()
+
+MimiDICE2016.set_param!(mm, :YGROSS, input_gdp)
+MimiDICE2016.set_param!(mm, :E, input_co2_emissions)
+
+run(mm)
+
+mm[:co2cycle, :E]
+m[:co2cycle, :E]
+
+# add_comp!(mm, Mimi.adder, :marginalemission, before=:co2cycle)
+
+year = 2030
+year_index = findfirst(isequal(year), model_years)
+
+# addem = zeros(length(model_years))
+# addem[year_index] = 1.0
+
+# set_param!(mm, :marginalemission, :add, addem)
+# connect_param!(mm, :marginalemission, :input, :emissions, :E)
+# connect_param!(mm, :co2cycle, :E, :marginalemission, :output)
+
+new_emissions = mm[:co2cycle, :E]
+new_emissions[year_index] =  mm[:co2cycle, :E][year_index] + (1.0 * 12/44)
+MimiDICE2016.set_param!(mm, :E, new_emissions)
+
+
+run(mm)
+
+
+mm[:co2cycle, :E] .- m[:co2cycle, :E]
+
+marginal_damages = (m[:neteconomy, :C] .- mm[:neteconomy, :C]) ./ (5*1e9) * 1e12
+
+
+
+plot(model_years, m[:neteconomy, :C], title = "Consumption", label = "Base", legend = :topleft)
+plot!(model_years, mm[:neteconomy, :C], label = "Marginal")
+
+plot(model_years, marginal_damages, title = "Marginal Damages", label = "Base", legend = :bottomright)
+
+plot(model_years, m[:co2cycle, :E], title = "Emissions", label = "Base", legend = :topright)
+plot!(model_years, mm[:co2cycle, :E], label = "Marginal")
+
+
+
+prtp = 0.03
+eta = 0.0
+
+annual_years = collect(2015:1:2510)
+
+md_interp = MimiIWG._interpolate(marginal_damages, model_years, annual_years)
+
+# constant discounting for now
+df = zeros(length(annual_years))
+for i in 1:length(annual_years)
+    if i >= year_index
+        df[i] = 1/(1+prtp)^(i-year_index)
+    end
+end
+
+scc = sum(df .* md_interp) # 81.1631605069015
+
+# sum(marginal_damages)
+
+
