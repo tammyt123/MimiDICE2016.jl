18c3283127e8d0e09197fff98106ec0d209b9535
Tammy Tan <46847428+tammyt123@users.noreply.github.com>
2021-08-26 10:07:49 -0400

    add function descriptions, remove old files

--- src/FAIR_implementation.jl
+++ src/FAIR_implementation.jl
@@ -1,12 +1,23 @@
-## function to get baseline DICE-FAIR model (i.e. input temperature vector from FAIR-NCEE into baseline DICE 2016)
-function get_dicefair(;usg_scenario::String)
+
+#######################################################################################################################
+# LOAD BASELINE DICEFAIR MODEL
+########################################################################################################################
+# Description: Return DICE2016 model with temperature vector set to baseline FAIR model. Note: in order to run this, FAIR-NCEE must
+#              be loaded in the environment.
+#
+# Function Arguments:
+#
+#       rcp_scenario:     RCP scenario ("RCP26", "RCP45", "RCP60" or "RCP85")
+#----------------------------------------------------------------------------------------------------------------------
+
+function get_dicefair(;rcp_scenario::String="RCP85")
     
     ## load baseline DICE model
     m = MimiDICE2016.get_model()
     run(m)
 
     ## load baseline FAIR-NCEE
-    FAIR = MimiFAIR.get_model(usg_scenario = usg_scenario)
+    FAIR = MimiFAIR.get_model(rcp_scenario = rcp_scenario)
     run(FAIR)
 
     fair_years = collect(1765:1:2300)
@@ -25,19 +36,32 @@ function get_dicefair(;usg_scenario::String)
 
 end
 
-## function to get marginal DICE-FAIR model (i.e. input perturbed FAIR temperature vector into DICE2016)
-## note: FAIR-NCEE must be loaded in environment!!
-function get_dicefair_marginal_model(;usg_scenario::String, pulse_year::Int)
+
+#######################################################################################################################
+# GET MARGINAL DICEFAIR MODEL
+########################################################################################################################
+# Description: Return marginal (perturbed) DICE2016 model with temperature vector set to perturbed FAIR model. 
+#              Note: in order to run this, FAIR-NCEE must be loaded in the environment.
+#
+# Function Arguments:
+#
+#       rcp_scenario:     RCP scenario ("RCP26", "RCP45", "RCP60" or "RCP85")
+#       gas:              Gas to perturb (:CO2, :CH4, or :N2O).
+#       pulse_year:       Pulse year (for SC-GHG calculation).
+#       pulse_size:       Pulse size (defaults to 1.0).
+#----------------------------------------------------------------------------------------------------------------------
+
+function get_dicefair_marginal_model(;rcp_scenario::String, pulse_year::Int, pulse_size::Float64=1.0, gas::Symbol=:CO2)
     
     ## create DICE2016 marginal model
-    m = MimiDICE2016.get_dicefair(usg_scenario = usg_scenario)
-    mm = Mimi.create_marginal_model(m, 1e9)
+    m = MimiDICE2016.get_dicefair(rcp_scenario = rcp_scenario)
+    mm = Mimi.create_marginal_model(m, pulse_size * 1e9)
     run(mm)
 
     ## get perturbed FAIR temperature vector
     fair_years = collect(1765:1:2300)
     dice2016_years = collect(2015:5:2510) # or use model_years
-    new_temperature = MimiFAIR.get_perturbed_fair_temperature(usg_scenario = usg_scenario, pulse_year = pulse_year)
+    new_temperature = MimiFAIR.get_perturbed_fair_temperature(rcp_scenario = rcp_scenario, pulse_year = pulse_year, pulse_size = pulse_size, gas = gas)
     new_temperature_df = DataFrame(year = fair_years, T = new_temperature)
 
     new_input_temp = new_temperature_df[[year in dice2016_years for year in fair_years], :T] # 58 element array, needs to be 100. make temperature constant after 2300
@@ -51,9 +75,24 @@ function get_dicefair_marginal_model(;usg_scenario::String, pulse_year::Int)
 
 end
 
-## compute SCC from DICE-FAIR
-function compute_scc_dicefair(;usg_scenario::String, pulse_year::Int, prtp::Float64, eta::Float64, last_year::Int=2300)
-    mm = MimiDICE2016.get_dicefair_marginal_model(usg_scenario = usg_scenario, pulse_year = pulse_year)
+
+#######################################################################################################################
+# COMPUTE SC-GHG
+########################################################################################################################
+# Description: Compute SC-GHGs. Note: in order to run this, FAIR-NCEE must be loaded in the environment.
+#
+# Function Arguments:
+#
+#       rcp_scenario:     RCP scenario ("RCP26", "RCP45", "RCP60" or "RCP85")
+#       prtp:             Pure rate of time preference, for calculating discount rate.
+#       eta:              Eta parameter, for calculating discount rate.
+#       gas:              Gas to perturb (:CO2, :CH4, or :N2O).
+#       pulse_year:       Pulse year (for SC-GHG calculation).
+#       pulse_size:       Pulse size (defaults to 1.0).
+#----------------------------------------------------------------------------------------------------------------------
+
+function compute_scghg_dicefair(;rcp_scenario::String="RCP85", pulse_year::Int, prtp::Float64, eta::Float64, last_year::Int=2300, pulse_size::Float64=1.0, gas::Symbol=:CO2)
+    mm = MimiDICE2016.get_dicefair_marginal_model(rcp_scenario = rcp_scenario, pulse_year = pulse_year, pulse_size = pulse_size, gas = gas)
     scc = MimiDICE2016._compute_scc(mm, year = pulse_year, last_year = last_year, prtp = prtp, eta = eta)
     return(scc)
 end
